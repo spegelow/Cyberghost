3597d99543286b821c539e7e0fc248edb2f865c0
Sawyer <sawyer.miller12@gmail.com>
2022-04-25 18:18:55 -0500

    Basic Battle UI
    Added UI for battle units

---
+++ Assets/Scripts/BattleUnitUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BattleUnitUI : MonoBehaviour
+{
+    public TMP_Text battleUnitUI;
+
+    public void UpdateUI(UnitManager unitManager)
+    {
+        battleUnitUI.text = "" + unitManager.data.unitName + " Health " + unitManager.currentHealth + "/ " + unitManager.data.baseHealth + "/nAction Cooldown: " + unitManager.actionCooldown;
+    }
+}
